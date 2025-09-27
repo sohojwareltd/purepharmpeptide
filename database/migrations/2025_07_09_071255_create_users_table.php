d6c1b01d048659ebbb473a1883be17dffc0f69d6
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2025-09-27 16:43:30 +0600

    asd

--- database/migrations/2025_07_09_071255_create_users_table.php
+++ database/migrations/2025_07_09_071255_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreignId('role_id')->constrained('roles');
+            $table->foreignId('role_id')->nullable()->constrained('roles');
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
@@ -24,8 +24,6 @@ return new class extends Migration
             $table->string('country')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->json('details')->nullable();
-            $table->boolean('is_wholesaler')->default(false);
-            $table->string('current_level')->default('retailer');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
