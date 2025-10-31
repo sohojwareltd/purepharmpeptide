50ff5fa20eda428bf36cc2232d48256bab5119f1
Shuvo <102598399+Shuvoakonda@users.noreply.github.com>
2025-10-31 15:04:21 +0600

    asdf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -179,7 +179,7 @@
             </div>
         </section>
 
-        <section class="contact-section py-5">
+        {{-- <section class="contact-section py-5">
             <div class="container">
                 <div class="row align-items-center justify-content-center text-center text-lg-start">
                     <div class="col-12 col-md-10 col-lg-6 mb-4 mb-lg-0 text-center">
@@ -193,7 +193,7 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> --}}
 
 
 
